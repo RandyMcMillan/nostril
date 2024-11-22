795478d295d80a6374bf7ea462595b5f80aea5d9
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-22 15:41:00 -0500

    ivendor:remove

--- vendor/gnostr-bins/README.md
+++
@@ -1,3 +0,0 @@
-# nostr-probe
-
-Some command-line tools for nostr.
--- vendor/gnostr-bins/build.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    Command::new("cargo").args(["update", "nostr-types"]);
-    Command::new("brew").args(["install", "openssl@1.1", "||", "true"]);
-    Command::new("apt").args(["install", "openssl", "||", "true"]);
-    Command::new("apk").args(["add", "openssl-dev", "||", "true"]);
-}
